b54d671598faca6dd6257b6678804604749713e3
Khulekani Ngongoma <khulekanik@live.com>
2014-03-03 11:19:08 -0800

    Adds server side code

---
+++ zombie/controllers/AccountController.js
@@ -0,0 +1,127 @@
+﻿module.exports = function (self,mongoose) {
+    var Account = require("../models/Account")(mongoose);
+
+    self.routes['/account/login/:username?/:password?'] = function (request,response) {
+
+        if (request.param("username") && request.param("password")) {
+            Account.login(request.param("username"), request.param("password"),response);
+        } else {
+            console.log("Username or password has not been set " + request.param("username") + " " + request.param("password"));
+            response.end("false");
+        }
+
+    };
+
+    self.routes['/account/register/:username?/:password?/:email_address?'] = function (request, response) {
+        console.log("request : "+request," response : "+response);
+        if (request.param("username") && request.param("password") && request.param("email_address")) {
+            Account.register(request.param("username"), request.param("password"), request.param("email_address"),response);
+        } else {
+            console.log("Username, password or email address has not been set " + request.param("username") + " " + request.param("password") + " " + request.param("email_address"));
+            response.end("false");
+        }
+
+    };
+
+    self.routes['/account/resetPassword/:email_address?'] = function (request,response) {
+       
+        if (request.param("email_address")) {
+            Account.resetPassword(request.param("email_address"),response);
+        } else {
+            console.log("Email address has not been set " + request.param("email_address"));
+            response.end("false");
+        }
+    };
+    
+
+    self.routes['/account/setPresence/:username?/:presence?'] = function (request,response) {
+
+        if (request.param("presence") && request.param("username")) {
+            Account.setPresence(request.param("username"),request.param("presense"),response);
+        } else {
+            console.log("username or presense is not set " + request.param("username") +" "+ request.param("presense"));
+            response.end("false");
+        }
+
+    };
+    
+
+
+    self.routes['/account/:inviter?/invites/:invitee?'] = function (request,response) {
+
+        if (request.param("inviter") && request.param("invitee")) {
+            Account.setInvite(request.param("inviter"),request.param("invitee"),response);
+        } else {
+            console.log("inviter or invitee is not set " + request.param("inviter") + " " + request.param("invitee"));
+            response.end("false");
+        }
+
+    };
+
+    self.routes['/account/:invitee?/accepts/:inviter?'] = function(request,response) {
+        if (request.param("invitee") && request.param("inviter")) {
+            Account.acceptInvite(request.param("invitee"),request.param("inviter"),response);
+        } else {
+            console.log("invitee or inviter is not set " + request.param("invitee") + " " + request.param("inviter"));
+            response.end("false");
+        }
+    };
+    
+    self.routes['/account/:invitee?/declines/:inviter?'] = function (request, response) {
+        if (request.param("invitee") && request.param("inviter")) {
+            Account.declineInvite(request.param("invitee"), request.param("inviter"), response);
+        } else {
+            console.log("invitee or inviter is not set " + request.param("invitee") + " " + request.param("inviter"));
+            response.end("false");
+        }
+    };
+
+    self.routes['/account/:username?/removeFriend/:friend?'] = function (request, response) {
+        
+        if (request.param("username") && request.param("friend")) {
+            Account.removeFriend(request.param("username"),request.param("friend"),response);
+        } else {
+            console.log("username or friend is not set " + request.param("username") + " " + request.param("friend"));
+            response.end("false");
+        }
+
+    };
+
+    self.routes['/account/friends/:username?'] = function (request, response) {
+
+        if (request.param("username")) {
+            Account.getUserFriends(request.param("username"),response);
+        } else {
+            console.log("username is not set " + request.param("username"));
+            response.end("false");
+        }
+
+
+    };
+    
+
+    self.routes['/account/friends/invites/:username?'] = function (request, response) {
+
+        if (request.param("username")) {
+            Account.getUserFriendsInvite(request.param("username"),response);
+        } else {
+            console.log("username is not set " + request.param("username"));
+            response.end("false");
+        }
+
+
+    };
+
+    self.routes['/account/profile/:username?'] = function(request,response) {
+
+        if (request.param("username")) {
+            Account.getUserProfile(request.param("username"),response);
+        } else {
+            console.log("username is not set " + request.param("username"));
+            response.end("false");
+        }
+
+
+    };
+
+};
---
+++ zombie/models/Account.js
@@ -0,0 +1,324 @@
+﻿
+module.exports = function (mongoose) {
+
+    var FriendsSchema = new mongoose.Schema({
+            username : {type : String, unique : true}
+    });
+
+    var Friends = mongoose.model("Friends",FriendsSchema);
+
+    var AccountSchema = new mongoose.Schema({
+        email_address: { type: String, unique: true },
+        username: { type: String, unique: true },
+        password: { type: String },
+        presence: { type: String, default: "offline" },
+        friends: [Friends],
+        friendInvites: [Friends],
+        name: {
+            first: { type: String },
+            last: { type: String }
+        },
+        birthday: {
+            day: { type: Number, min: 1, max: 31, required: false },
+            month: { type: Number, min: 1, max: 31, required: false },
+            year: { type: Number }
+        },
+        sex: { type: String },
+        city: { type: String },
+        country : { type : String},
+        photoData : { type : String },
+        biography : { type : String }
+    });
+
+   var Account = mongoose.model('Account', AccountSchema);
+   
+   this.login = function (_username,_password,response) {
+       
+       try {
+
+           Account.findOne({ username: _username, password: _password }, function(err, doc) {
+               if (err == null && doc != null) {
+                   console.log("Login success");
+                   response.end("true");
+               } else {
+                   console.log("Login error "+ err);
+                   response.end("false");
+               }
+           });
+
+       } catch (e) {
+           
+           console.log("Error : " + e);
+           response.end("false");
+       }
+   };
+
+
+   this.register = function (_username,_password,_email_address,response) {
+       console.log("In register function");
+       try {
+
+           var user = new Account({
+               username: _username,
+               password: _password,
+               email_address: _email_address
+           });
+
+           user.save(function (err) {
+               if (err) {
+                   console.log("Error : " + err);
+                   response.end("false");
+               } else {
+                   console.log("Success!");
+                   response.end("true");
+               }
+               console.log("In save function");
+           });
+ 
+       } catch (e) {
+           response.end("false");
+           console.log("Error : "+e);
+       }
+   };
+
+
+   this.resetPassword = function (email_address,response) {
+
+
+       response.end();
+   };
+
+   this.setPresence = function (_username,_presense,response) {
+
+       Account.findOne({ username : _username }, function (err,doc) {
+           if (err == null && doc != null) {
+               doc.presence = _presense;
+               doc.save(function(err) {
+                   if (err == null) {
+                       
+                       response.end("true");
+                       
+                   } else {
+                       
+                       console.log("Error : "+err);
+                       response.end("false");
+                   }
+               });
+           } else {
+               
+               console.log("Error : "+err);
+               response.end("false");
+               
+           }
+       });
+
+   };
+
+   this.setInvite = function (inviter,invitee,response) {
+
+       Account.findOne({username : invitee}, function(err,doc) {
+           if (err == null && doc != null) {
+               var hasAlreadyInvited = false;
+               doc.friendInvites.forEach(function(friendInvite) {
+                   if (inviter == friendInvite) {
+                       hasAlreadyInvited = true;
+                       
+                   }
+                   
+               });
+               var isAlreadyFriend = false;
+               doc.friends.forEach(function(friend) {
+                   if (inviter == friend) {
+                       isAlreadyFriend = true;
+                      
+                   }
+               });
+
+
+               if (!hasAlreadyInvited && !isAlreadyFriend) {
+                   doc.friendInvites.push({ username: inviter });
+                   doc.save(function(err) {
+                       if (err != null) {
+                           response.end("true");
+                       } else {
+                           console.log("Error : " + err);
+                           response.end("false");
+                       }
+                   });
+               }
+
+           } else {
+               console.log("Error : "+err);
+               response.end("false");
+           }
+       });
+
+   };
+
+   this.acceptInvite = function(invitee,inviter,response) {
+
+       Account.findOne({username : invitee},function(err,doc) {
+           if (err == null && doc != null) {
+               for (var i = 0; i < doc.friendInvites.length; i++) {
+                   if (doc.friendInvites[i].username == inviter) {
+                       doc.friends.push({ username: inviter });
+                       doc.friendInvites[i].remove();
+                       doc.save(function(err) {
+                           if (err == null) {
+                               response.end("true");
+                           } else {
+                               console.log("Error : " + err);
+                               response.end("false");
+                           }
+
+                       });
+                   }
+               }
+           } else {
+               console.log("Error : "+err);
+               response.end("false");
+           }
+       });
+
+   };
+    
+    
+   this.declineInvite = function(invitee,inviter,response) {
+
+       Account.findOne({username : invitee},function(err,doc) {
+           if (err == null && doc != null) {
+               for (var i = 0; i < doc.friendInvites.length; i++) {
+                   if (doc.friendInvites[i].username == inviter) {
+                       doc.friendInvites[i].remove();
+                       doc.save(function(err) {
+                           if (err == null) {
+                               response.end("true");
+                           } else {
+                               console.log("Error : " + err);
+                               response.end("false");
+                           }
+                       });
+                   }
+               }
+           } else {
+               console.log("Error : "+err);
+               response.end("false");
+           }
+       });
+
+   };
+
+    this.removeFriend = function(_username,friend,response) {
+
+        Account.findOne({username : _username},function(err,doc) {
+            if (err == null && doc != null) {
+                for (var i = 0; i < doc.friends.length; i++) {
+                    if (doc.friends[i].username == friend) {
+                        doc.friends[i].remove();
+                        doc.save(function(err) {
+                            if (err != null) {
+                                console.log("Error : " + err);
+                                response.end("false");
+                            }
+                        });
+                    }
+                }
+            } else {
+                console.log("Error : "+err);
+                response.end("false");
+            }
+        });
+        
+        Account.findOne({username : friend},function(err,doc) {
+            if (err == null && doc != null) {
+                for (var i = 0; i < doc.friends.length; i++) {
+                    if (doc.friends[i].username == username) {
+                        doc.friends[i].remove();
+                        doc.save(function(err) {
+                            if (err != null) {
+                                console.log("Error : " + err);
+                                response.end("false");
+                            } else {
+                                response.end("true");
+                            }
+                        });
+                    }
+                }
+            } else {
+                console.log("Error : "+err);
+                response.end("false");
+            }
+        });
+
+    };
+
+    this.getUserFriends = function(_username,response) {
+
+        Account.findOne({username : _username},function(err,doc) {
+            if (err == null && doc != null) {
+                console.log("friends about to be returned "+response);
+                response.end(JSON.stringify(doc.friends));
+            } else {
+                console.log("An error occured while trying to get the user's friends list");
+                console.log("Error : " + err.toString());
+                response.end("false");
+            }
+        });
+
+
+    };
+    
+    this.getUserFriendsInvites = function (_username, response) {
+
+        Account.findOne({ username: _username }, function (err, doc) {
+            if (err == null && doc != null) {
+                console.log("friends invites about to be returned "+response);
+                response.end(JSON.stringify(doc.friendInvites));
+            } else {
+                console.log("An error occured while trying to get the user's friends invite list");
+                console.log("Error : " + err.toString());
+                response.end("false");
+            }
+        });
+    };
+
+
+    this.getUserProfile = function (_username, response) {
+
+        var userProfileInfo = {
+            "firstName" : "TestName" ,
+            "lastName" : "TestLastName",
+            "birthday" : "TestBirthday",
+            "sex" : "TestSex",
+            "city" : "TestCity",
+            "country" : "TestCountry",
+            "biography" : "TestBiography",
+            "photoData" : "TestPhoto"
+        };
+
+        Account.findOne({ username : _username},function(err,doc) {
+            if (err == null && doc != null) {
+
+                userProfileInfo.firstName = doc.name.first;
+                userProfileInfo.lastName = doc.name.last;
+                userProfileInfo.birthday += doc.birthday.day;
+                userProfileInfo.birthday += doc.birthday.month;
+                userProfileInfo.birthday += doc.birthday.year;
+                userProfileInfo.sex = doc.sex;
+                userProfileInfo.city = doc.city;
+                userProfileInfo.country = doc.country;
+                userProfileInfo.photoData = doc.photoData;
+
+                response.end(JSON.stringify(userProfileInfo));
+
+            } else {
+                console.log("Error getting user profile");
+                console.log("Error : " + err.toString());
+                response.end("false");
+            }
+        });
+
+    };
+
+    return this;
+};
